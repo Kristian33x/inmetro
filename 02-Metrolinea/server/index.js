fe86aeb22c1010b257c7043197776ebac3c6b083
Cristian Fraija <cristian33x@gmail.com>
2020-02-15 17:47:20 -0500

    funciona1

--- 02-Metrolinea/server/index.js
+++ 02-Metrolinea/server/index.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const cors = require('cors');
-const { mongoose } = require('./database');
+// const cors = require('cors');
+// const { mongoose } = require('./database');
 
 
 // Settings
 app.set('port', process.env.PORT || 3001);
 
 // Middlewares
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(cors({ origin: 'http://localhost:4200' }))
+// app.use(morgan('dev'));
+// app.use(express.json());
+// app.use(cors({ origin: 'http://localhost:4200' }))
 
-// Routes
-app.use('/api/buses', require('./routes/bus.routes'));
-app.use('/api/conductores', require('./routes/conductor.routes'));
+// // Routes
+// app.use('/api/buses', require('./routes/bus.routes'));
+// app.use('/api/conductores', require('./routes/conductor.routes'));
 
 //Starting the server
 // const server = 
-app.listen(app.get('port'), () => {
-    console.log('server running on port ', app.get('port'))
-});
+// app.listen(app.get('port'), () => {
+//     console.log('server running on port ', app.get('port'))
+// });
 
 
 // const SocketIO = require('socket.io');
